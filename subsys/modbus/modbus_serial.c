8d9e9c7c4babf09b65c94768db7a5826418459d8
Erik Kallen <info@erikkallen.nl>
2021-10-19 16:59:05 +0200

    fix rx tx race condition when both fire at the same time

--- subsys/modbus/modbus_serial.c
+++ subsys/modbus/modbus_serial.c
@@ -414,13 +414,11 @@ static void uart_cb_handler(const struct device *dev, void *app_data)
 	cfg = ctx->cfg;
 
 	while (uart_irq_update(cfg->dev) && uart_irq_is_pending(cfg->dev)) {
-
-		if (uart_irq_rx_ready(cfg->dev)) {
-			cb_handler_rx(ctx);
-		}
-
+		// Handle tx requests first in case both rx and tx fire at the same time
 		if (uart_irq_tx_ready(cfg->dev)) {
 			cb_handler_tx(ctx);
+		} else if (uart_irq_rx_ready(cfg->dev)) {
+			cb_handler_rx(ctx);
 		}
 	}
 }
